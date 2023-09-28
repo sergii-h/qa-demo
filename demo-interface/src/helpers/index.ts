ca66f7ebdc514757e1b45f7f02a463abdba96cbc
serhii.holdys <serhii.holdys@lottoland.com>
2023-09-28 17:48:26 +0200

    1 add variable to point to specific BE Api url

--- demo-interface/src/helpers/index.ts
+++ demo-interface/src/helpers/index.ts
@@ -1,29 +1,31 @@
 import IItem from "../interfaces/IItem";
 
+const beApi = process.env.REACT_APP_BE_API || "http://localhost:8080/v1";
+
 const getItems = async (): Promise<IItem[]> => {
-    const response = await fetch(`http://localhost:8080/v1/item`, { method: 'GET', headers: { 'Content-Type': 'application/json'} });
+    const response = await fetch(beApi + `/item`, { method: 'GET', headers: { 'Content-Type': 'application/json'} });
 
     return await response.json();
 }
 
 const getItem = async (itemId: string): Promise<IItem> => {
-    const response = await fetch(`http://localhost:8080/v1/item/${itemId}`, { method: 'GET', headers: { 'Content-Type': 'application/json'} });
+    const response = await fetch(beApi + `/item/${itemId}`, { method: 'GET', headers: { 'Content-Type': 'application/json'} });
 
     return await response.json();
 }
 
 const createItem = async (item: IItem): Promise<IItem> => {
-    const response = await fetch(`http://localhost:8080/v1/item`, { method: 'POST', body: JSON.stringify({ ...item }), headers: { 'Content-Type': 'application/json'} });
+    const response = await fetch(beApi + `/item`, { method: 'POST', body: JSON.stringify({ ...item }), headers: { 'Content-Type': 'application/json'} });
 
     return await response.json();
 }
 
 const updateItem = async (item: IItem): Promise<any> => {
-    return await fetch(`http://localhost:8080/v1/item/${item.id}`, { method: 'PUT', body: JSON.stringify({ name: item.name, description: item.description }), headers: { 'Content-Type': 'application/json'} });
+    return await fetch(beApi + `/item/${item.id}`, { method: 'PUT', body: JSON.stringify({ name: item.name, description: item.description }), headers: { 'Content-Type': 'application/json'} });
 }
 
 const deleteItem = async (itemId: string): Promise<any> => {
-    return await fetch(`http://localhost:8080/v1/item/${itemId}`, { method: 'DELETE', headers: { 'Content-Type': 'application/json'} });
+    return await fetch(beApi + `/item/${itemId}`, { method: 'DELETE', headers: { 'Content-Type': 'application/json'} });
 }
 
 export  {
