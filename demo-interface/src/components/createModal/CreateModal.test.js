dfdf528fe54babc254faa4e40e258f9f3026a772
serhii.holdys <serhii.holdys@lottoland.com>
2024-01-12 13:16:52 +0100

    qa-demo-5 add CreateModal tests

---
+++ demo-interface/src/components/createModal/CreateModal.test.js
@@ -0,0 +1,105 @@
+import {fireEvent, render, screen, waitFor} from '@testing-library/react';
+import {CreateModal} from './createModal';
+import * as React from 'react';
+import {jest} from '@jest/globals'
+import {mockCreateItem} from '../../mocks/mockFetch';
+import {BE_API} from "../../helpers";
+
+let windowFetchSpy;
+
+beforeEach(() => {
+    windowFetchSpy = jest.spyOn(window, 'fetch').mockImplementation(mockCreateItem);
+})
+
+it('should render with default values', () => {
+    // when
+    render(<CreateModal />)
+
+    // then
+    expect(screen.getByLabelText('Name')).toHaveProperty('value', '')
+    expect(screen.getByLabelText('Amount')).toHaveProperty('value', "0")
+    expect(screen.getByLabelText('Description')).toHaveProperty('value', '')
+});
+
+it('should render spinner when loading', () => {
+    // when
+    render(<CreateModal isLoading={true}/>)
+
+    // then
+    expect(document.querySelector('.p-progress-spinner')).toBeVisible()
+    expect(screen.queryByLabelText('Name')).toBeNull()
+});
+
+it('should change values', () => {
+    // given
+    render(<CreateModal />)
+
+    const nameElement = screen.getByLabelText('Name')
+    const amountElement = screen.getByLabelText('Amount')
+    const descriptionElement = screen.getByLabelText('Description')
+
+    // when
+    fireEvent.change(nameElement, {target: {value: "name"}})
+    fireEvent.change(amountElement, {target: {value: 1}})
+    fireEvent.change(descriptionElement, {target: {value: "description"}})
+
+    // then
+    expect(nameElement).toHaveProperty('value', "name")
+    expect(amountElement).toHaveProperty('value', "1")
+    expect(descriptionElement).toHaveProperty('value', "description")
+});
+
+[
+    { name: "name",         amount: 1,   description: "description"  },
+    { name: "first second", amount: -1,  description: "first second" },
+    { name: " ",            amount: 0,   description: " "            },
+    { name: "",             amount: 0.5, description: ""             },
+].forEach((data) => {
+    it(`should post /item with parameters { '${data.name}', ${data.amount}, '${data.description}' }`, async () => {
+        // given
+        render(<CreateModal onClose={() => true} onSave={() => true}/>)
+
+        const nameElement = screen.getByLabelText('Name')
+        const amountElement = screen.getByLabelText('Amount')
+        const descriptionElement = screen.getByLabelText('Description')
+
+        fireEvent.change(nameElement, {target: {value: data.name}})
+        fireEvent.change(amountElement, {target: {value: data.amount}})
+        fireEvent.change(descriptionElement, {target: {value: data.description}})
+
+        // when
+        await waitFor(() => {
+            screen
+                .getAllByRole('button')
+                .find(element => element.className === 'p-button p-component create-button')
+                .click()
+        })
+
+        // then
+        await waitFor(() => {
+            expect(windowFetchSpy).toHaveBeenCalledWith(
+                `${BE_API}/item`,
+                expect.objectContaining({
+                    "body": `{"name":"${data.name}","amount":${data.amount},"description":"${data.description}"}`,
+                    "method": "POST"}
+                )
+            )
+        })
+    })
+})
+
+it("should not post /item when Close button is clicked", async () => {
+    // given
+    render(<CreateModal onClose={() => true} onSave={() => true}/>)
+
+    // when
+    await waitFor(() => {
+        screen
+            .getAllByRole('button')
+            .find(element => element.className.includes('close-button'))
+            .click()
+    })
+
+    // then
+    expect(windowFetchSpy).not.toHaveBeenCalled()
+})
