d236ea63aa99a1d70a5d80110a1a2b02c4d53edf
serhii.holdys <serhii.holdys@lottoland.com>
2024-03-12 08:39:02 +0100

    qa-demo-10 extend FE testing coverage

--- demo-interface/src/components/infoModal/InfoModal.test.js
+++ demo-interface/src/components/infoModal/InfoModal.test.js
@@ -1,45 +1,139 @@
-import {act, render, screen} from '@testing-library/react';
+import {act, render, within} from '@testing-library/react';
 import * as React from 'react';
 import {jest} from '@jest/globals'
-import {itemResponse, mockFetch} from '../../mocks/mockFetch';
 import {InfoModal} from "./infoModal";
+import {BE_API} from "../../helpers";
 
-let fetchSpy;
+const title = () => within(document.querySelector('.p-dialog-title'))
+const amount = () => within(document.querySelector('#info-modal_content > p:nth-child(2)'))
+const description = () => within(document.querySelector('#info-modal_content > p:last-child'))
+
+const MockFetch = require('../../mocks/mockFetch').default;
+const spyOnFetch = jest.spyOn(window, 'fetch')
+
+let mockFetch
+let fetchSpy
 
 beforeEach(() => {
-    itemResponse.body = {
-        id: '1',
-        name: 'name1',
-        description: 'description1',
-        amount: '1',
-    }
-
-    fetchSpy = jest.spyOn(window, 'fetch').mockImplementation(mockFetch);
+    mockFetch = new MockFetch()
+    fetchSpy = spyOnFetch.mockImplementation(mockFetch.execute)
 });
 
-it('should render with mocked values', async () => {
+[
+    { name: "name",         amount: 1,     description: "description"         },
+    { name: "name",         amount: 0.5,   description: "description"         },
+    { name: "name",         amount: "0,5", description: "description"         },
+    { name: "another name", amount: -1,    description: "another description" },
+    { name: " ",            amount: 0,     description: " "                   },
+    { name: "name",         amount: "",    description: ""                    },
+    { name: "name",         amount: "01",  description: ""                    },
+].forEach((data) => {
+    it(`should render with values { '${data.name}', ${data.amount}, '${data.description}' }`, async () => {
+        // given
+        mockFetch = new MockFetch({
+            itemResponse: {
+                status: 200,
+                body: {
+                    id: '1',
+                    name: data.name,
+                    description: data.description,
+                    amount: data.amount,
+                }
+            }
+        })
+
+        spyOnFetch.mockImplementation(mockFetch.execute)
+
+        // when
+        await act(() => render(<InfoModal itemId={mockFetch.payload.itemResponse.body.id} />))
+
+        // then
+        expect(title().getByText(mockFetch.payload.itemResponse.body.name.trim())).toBeVisible()
+        expect(amount().getByText((mockFetch.payload.itemResponse.body.amount + ' €').trim())).toBeVisible()
+        expect(description().getByText(mockFetch.payload.itemResponse.body.description.trim())).toBeVisible()
+    })
+});
+
+[
+    { id: '1',               amount: 1,    description: "description" },
+    { id: '1', name: 'name',               description: "description" },
+    { id: '1', name: 'name', amount: 1                                },
+].forEach((body) => {
+    it('should render default values when absent in response', async () => {
+        // given
+        mockFetch = new MockFetch({
+            itemResponse: {
+                status: 200,
+                body: body
+            }
+        })
+
+        spyOnFetch.mockImplementation(mockFetch.execute)
+
+        // when
+        await act(() => render(<InfoModal itemId={mockFetch.payload.itemResponse.body.id} />))
+
+        // then
+        expect(title().getByText(mockFetch.payload.itemResponse.body.name || 'Info')).toBeVisible()
+        expect(amount().getByText(`${mockFetch.payload.itemResponse.body.amount || ''} €`.trim())).toBeVisible()
+        expect(description().getByText(mockFetch.payload.itemResponse.body.description || '')).toBeVisible()
+    })
+});
+
+it(`should get /item with parameter`, async () => {
     // when
-    await act(() => render(<InfoModal itemId={itemResponse.body.id} />))
+    await act(() => render(<InfoModal itemId={mockFetch.payload.itemResponse.body.id} />))
 
     // then
     expect(fetchSpy).toHaveBeenCalledTimes(1)
 
-    expect(screen.getByText(itemResponse.body.name)).toBeVisible()
-    expect(screen.getByText(itemResponse.body.amount + ' €')).toBeVisible()
-    expect(screen.getByText(itemResponse.body.description)).toBeVisible()
+    expect(fetchSpy).toHaveBeenCalledWith(
+        `${BE_API}/item/${mockFetch.payload.itemResponse.body.id}`,
+        expect.objectContaining({"method": "GET"})
+    )
 });
 
-it('should render default name if absent in response', async () => {
+it('should render default values when error', async () => {
     // given
-    itemResponse.body = {
-        id: '1',
-        description: 'description1',
-        amount: '1',
-    }
+    mockFetch = new MockFetch({
+        itemResponse: {
+            status: 400,
+            body: {}
+        }
+    })
+
+    spyOnFetch.mockImplementation(mockFetch.execute)
 
     // when
-    await act(() => render(<InfoModal itemId={itemResponse.body.id} />))
+    await act(() => render(<InfoModal itemId={'1'} />))
 
     // then
-    expect(screen.getByText('Info')).toBeVisible()
-});
+    expect(title().getByText('Info')).toBeVisible()
+    expect(amount().getByText('€')).toBeVisible()
+    expect(description().getByText('')).toBeVisible()
+})
+
+it('should render received values when wrong id', async () => {
+    // given
+    mockFetch = new MockFetch({
+        itemResponse: {
+            status: 200,
+            body: {
+                id: '1',
+                name: 'name',
+                description: 'description',
+                amount: 1,
+            }
+        }
+    })
+
+    spyOnFetch.mockImplementation(mockFetch.execute)
+
+    // when
+    await act(() => render(<InfoModal itemId={'2'} />))
+
+    // then
+    expect(title().getByText(mockFetch.payload.itemResponse.body.name.trim())).toBeVisible()
+    expect(amount().getByText((mockFetch.payload.itemResponse.body.amount + ' €').trim())).toBeVisible()
+    expect(description().getByText(mockFetch.payload.itemResponse.body.description.trim())).toBeVisible()
+})
