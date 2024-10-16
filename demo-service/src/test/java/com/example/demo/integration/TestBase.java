cd3e20f75b31c4843eafddb3f55b006d9294248d
sgoldys <32680292+sgoldys@users.noreply.github.com>
2024-10-16 07:29:54 +0200

    qa-demo-19 set default awaitility timeout

--- demo-service/src/test/java/com/example/demo/integration/TestBase.java
+++ demo-service/src/test/java/com/example/demo/integration/TestBase.java
@@ -4,10 +4,14 @@ import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.time.Duration;
 
 import static io.restassured.RestAssured.given;
 
@@ -18,6 +22,11 @@ public class TestBase {
     int port;
 
     public RequestSpecification requestSpec;
+
+    @BeforeAll
+    public static void baseBeforeAll() {
+        Awaitility.setDefaultTimeout(Duration.ofMinutes(1));
+    }
 
     @BeforeEach
     public void baseBeforeEach() {
