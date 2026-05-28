aacfd50ea97d99096250e04f3af496a6157ccba9
sgoldys <32680292+sgoldys@users.noreply.github.com>
2026-05-28 10:02:55 +0200

    qa-demo-42 fix pact

--- demo-service/src/test/java/com/example/demo/integration/test/pact/api/GetIsValidPactProviderTest.java
+++ demo-service/src/test/java/com/example/demo/integration/test/pact/api/GetIsValidPactProviderTest.java
@@ -3,30 +3,27 @@ package com.example.demo.integration.test.pact.api;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
 import com.example.demo.data.Task;
-import com.github.tomakehurst.wiremock.WireMockServer;
-import org.junit.jupiter.api.parallel.ResourceLock;
 import org.wiremock.spring.ConfigureWireMock;
 import org.wiremock.spring.EnableWireMock;
-import org.wiremock.spring.InjectWireMock;
 
 import java.util.Map;
 import java.util.UUID;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 
 @Provider("demo-service-tasks-get-is-valid")
-@EnableWireMock(@ConfigureWireMock(port = 8085))
-@ResourceLock("wiremock-8085")
+@EnableWireMock(@ConfigureWireMock(
+        port = 0,
+        baseUrlProperties = "external.service.url"
+))
 public class GetIsValidPactProviderTest extends PactProviderTestBase {
-
-    @InjectWireMock
-    private WireMockServer wireMockServer;
 
     @State("validation result is true for the task")
     Map<String, Object> validationResultIsTrue() {
         Task task = taskRepository.save(buildTask(UUID.randomUUID().toString()));
-        wireMockServer.stubFor(post("/external/validate/task")
+        stubFor(post("/external/validate/task")
                 .willReturn(aResponse().withHeader("Content-Type", "application/json").withBody("true")));
         return Map.of("taskId", task.getId());
     }
