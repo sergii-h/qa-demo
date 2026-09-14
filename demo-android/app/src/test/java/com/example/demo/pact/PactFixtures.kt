a0e1a297710d9d3141419b32315f7883793f0af7
Sergii <32680292+sergii-h@users.noreply.github.com>
2026-09-14 10:35:08 +0200

    qa-demo-148 add type locking for demo-android

--- demo-android/app/src/test/java/com/example/demo/pact/PactFixtures.kt
+++ demo-android/app/src/test/java/com/example/demo/pact/PactFixtures.kt
@@ -4,6 +4,8 @@ import au.com.dius.pact.consumer.MockServer
 import au.com.dius.pact.consumer.dsl.DslPart
 import au.com.dius.pact.consumer.dsl.LambdaDsl
 import au.com.dius.pact.consumer.dsl.PactDslJsonBody
+import com.example.demo.data.model.ErrorResponse
+import com.example.demo.data.model.Task
 import com.example.demo.data.model.TaskPriority
 import com.example.demo.data.model.TaskRequest
 import com.example.demo.data.model.TaskStatus
@@ -33,41 +35,41 @@ object PactFixtures {
     )
 
     fun createTaskRequestBody(): DslPart = PactDslJsonBody()
-        .valueFromProviderState("title", "taskTitle", createTaskRequest().title)
-        .stringValue("description", createTaskRequest().description!!)
-        .stringValue("status", createTaskRequest().status.name)
-        .stringValue("priority", createTaskRequest().priority.name)
+        .valueFromProviderState(TaskRequest::title.name, "taskTitle", createTaskRequest().title)
+        .stringValue(TaskRequest::description.name, createTaskRequest().description!!)
+        .stringValue(TaskRequest::status.name, createTaskRequest().status.name)
+        .stringValue(TaskRequest::priority.name, createTaskRequest().priority.name)
 
     fun updateTaskRequestBody(): DslPart = PactDslJsonBody()
-        .valueFromProviderState("title", "updatedTitle", updateTaskRequest().title)
-        .stringValue("description", updateTaskRequest().description!!)
-        .stringValue("status", updateTaskRequest().status.name)
-        .stringValue("priority", updateTaskRequest().priority.name)
+        .valueFromProviderState(TaskRequest::title.name, "updatedTitle", updateTaskRequest().title)
+        .stringValue(TaskRequest::description.name, updateTaskRequest().description!!)
+        .stringValue(TaskRequest::status.name, updateTaskRequest().status.name)
+        .stringValue(TaskRequest::priority.name, updateTaskRequest().priority.name)
 
     fun taskResponseBody(titleExample: String = "Prepare release notes"): DslPart = PactDslJsonBody()
-        .stringMatcher("id", "^[a-f0-9]{24}$", TASK_ID)
-        .stringType("title", titleExample)
-        .stringType("description", "Document release tasks")
-        .stringType("status", TaskStatus.TODO.name)
-        .stringType("priority", TaskPriority.MEDIUM.name)
-        .stringMatcher("createdDate", TIMESTAMP_PATTERN, "2026-04-26T09:00:00.000Z")
-        .stringMatcher("updatedDate", TIMESTAMP_PATTERN, "2026-04-26T09:00:00.000Z")
+        .stringMatcher(Task::id.name, "^[a-f0-9]{24}$", TASK_ID)
+        .stringType(Task::title.name, titleExample)
+        .stringType(Task::description.name, "Document release tasks")
+        .stringType(Task::status.name, TaskStatus.TODO.name)
+        .stringType(Task::priority.name, TaskPriority.MEDIUM.name)
+        .stringMatcher(Task::createdDate.name, TIMESTAMP_PATTERN, "2026-04-26T09:00:00.000Z")
+        .stringMatcher(Task::updatedDate.name, TIMESTAMP_PATTERN, "2026-04-26T09:00:00.000Z")
 
     fun taskListResponseBody(): DslPart = LambdaDsl.newJsonArrayMinLike(1) { array ->
         array.`object` { item ->
-            item.stringMatcher("id", "^[a-f0-9]{24}$", TASK_ID)
-            item.stringType("title", "Prepare release notes")
-            item.stringType("description", "Document release tasks")
-            item.stringType("status", TaskStatus.TODO.name)
-            item.stringType("priority", TaskPriority.MEDIUM.name)
-            item.stringMatcher("createdDate", TIMESTAMP_PATTERN, "2026-04-26T09:00:00.000Z")
-            item.stringMatcher("updatedDate", TIMESTAMP_PATTERN, "2026-04-26T09:00:00.000Z")
+            item.stringMatcher(Task::id.name, "^[a-f0-9]{24}$", TASK_ID)
+            item.stringType(Task::title.name, "Prepare release notes")
+            item.stringType(Task::description.name, "Document release tasks")
+            item.stringType(Task::status.name, TaskStatus.TODO.name)
+            item.stringType(Task::priority.name, TaskPriority.MEDIUM.name)
+            item.stringMatcher(Task::createdDate.name, TIMESTAMP_PATTERN, "2026-04-26T09:00:00.000Z")
+            item.stringMatcher(Task::updatedDate.name, TIMESTAMP_PATTERN, "2026-04-26T09:00:00.000Z")
         }
     }.build()
 
     fun duplicateTitleErrorBody(exampleMessage: String): DslPart = PactDslJsonBody()
         .stringMatcher(
-            "message",
+            ErrorResponse::message.name,
             "^Task with title '.*' already exists$",
             exampleMessage
         )
